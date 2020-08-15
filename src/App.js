0f79b6103cafe64722685c1c4de0c172c7802a94
martrohelsanchez <martrohel.sanchez@my.jru.edu>
2020-08-15 16:40:36 +0800

    added my name

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import styles from './app.module.css';
 import First from './components/first/First';
 import Second from './components/second/Second';
+import Footer from './components/footer/Footer';
 
 function App() {
     return (
         <div className={styles.appCont}>
             <First />
             <Second />
+            <Footer />
         </div>
     )
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import styles from './footer.module.css';
+
+function Footer() {
+    return (
+        <div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <span>
+                Created by{' '}
+            </span>
+            <a href='https://github.com/martrohelsanchez' target='_blank'>
+                Mart Rohel Sanchez
+            </a>
+        </div>
+    )
+}
+
+export default Footer;
